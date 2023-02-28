0b044a305d96b5682d057d52cd9520b8c426eee5
everless-ai2 <126610466+everless-ai2@users.noreply.github.com>
2023-02-28 12:54:11 -0600

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,6 +0,0 @@
-# Today is feb. 28
-## #National Public Sleeping Day.
-
-### [Google](https://google.com)  <---Click here
-
-
---
+++ README.py
@@ -0,0 +1,7 @@
+print (Today is Feb. 28)
+print (National Public Sleeping Day.)
+Google = https://google.com
+link(Google), print (<--Click Here)
+
+
+
